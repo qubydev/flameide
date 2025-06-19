97398778b6eebb687175804527d731bbe0a9851d
Malay Patra <107231583+qubydev@users.noreply.github.com>
2025-06-20 00:21:56 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const LANGUAGES = {
     name: 'C++',
     extension: '.cpp',
     mode: 'cpp',
-    snippet: `#include <iostream>
+    snippet: `#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
